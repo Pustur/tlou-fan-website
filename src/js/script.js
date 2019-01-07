b582cf8367a370b100597392e7ac68337ed5471c
Loris Bettazza <loris.bettazza@gmail.com>
2019-01-07 22:23:15 +0100

    Continued awards section with carousels

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,40 @@
-(() => {
-  console.log('Script works');
-})();
+$(() => {
+  // Functions
+  function countUp() {
+    counter = (counter + 1) % $allItems.get(0).length;
+    animateScroll($scrollList);
+    applyClasses(counter);
+  }
+
+  function applyClasses(index) {
+    $allItems.each((_, items) => {
+      $(items)
+        .removeClass('active')
+        .eq(index)
+        .addClass('active');
+    });
+  }
+
+  function animateScroll($list) {
+    const marginBetweenItems = 10;
+    const itemHeight = $list
+      .find('li')
+      .eq(0)
+      .height();
+
+    $list.stop(true, true).animate({
+      scrollTop: `${itemHeight * counter + marginBetweenItems * counter}px`,
+    });
+  }
+
+  // DOM elements
+  const $lists = $('.js-awards-list');
+  const $scrollList = $lists.filter('.awards__carousel-list');
+  const $allItems = $lists.map((_, list) => $(list).find('li'));
+
+  // Variables
+  let counter = 0;
+
+  // Body
+  setInterval(countUp, 2500);
+});
